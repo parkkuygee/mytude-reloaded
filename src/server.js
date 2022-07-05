db2cc33c531d73a98c8518235b6aec1f5fdf6cfa
ParkKyuhee <pkhee389@naver.com>
2022-07-06 06:38:02 +0900

    loginSessions
    2022/07/06
    server.jsSetting_sessions

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./router/rootRouter";
 import videoRouter from "./router/videoRouter";
 import userRouter from "./router/userRouter";
@@ -16,6 +17,15 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({extended:true}));
+
+app.use(
+    session({
+        secret: "Hello",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
 app.use("/",rootRouter);
 app.use("/videos",videoRouter);
 app.use("/users",userRouter);
