3b2337056b75204114b3fdf8b7a4ec038058193e
ParkKyuhee <pkhee389@naver.com>
2022-07-14 07:03:50 +0900

    LoggedIn
    2022/7/14

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,6 @@
 export const localsMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn);
-    console.log(res.locals);
     res.locals.siteName= "Mytube";
+    res.locals.loggedInUser = req.session.user;
     next();
 };
--- src/server.js
+++ src/server.js
@@ -28,6 +28,7 @@ app.use(
 );
 
 
+
 app.use(localsMiddleware);
 app.use("/",rootRouter);
 app.use("/videos",videoRouter);
