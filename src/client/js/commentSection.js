6a1fba3132eaa0a2204926d4b4d2f5c141f52b2c
ParkKyuhee <pkhee389@naver.com>
2022-09-07 01:30:53 +0900

    220907
    local finish

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,13 +1,42 @@
+const { async } = require("regenerator-runtime");
+
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
+const textarea = form.querySelector("textarea");
+const deleteBtn = document.querySelectorAll(".deleteBtn");
 
-const handleSubmit = (event) => {
+const addComment = (text, id) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.dataset.id = id;
+  newComment.className = "video__comment";
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = ` ${text}`;
+  const span2 = document.createElement("span");
+  span2.innerText = "❌";
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  newComment.appendChild(span2);
+  videoComments.prepend(newComment);
+};
+
+const deleteComment = (event) => {
+  const commentContainer = document.querySelector(".video__comments ul");
+  const commentList = event.target.parentNode;
+  commentContainer.removeChild(commentList);
+};
+
+const handleSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
   const text = textarea.value;
   const videoId = videoContainer.dataset.id;
-  console.log(videoId);
-  fetch(`/api/videos/${videoId}/comment`, {
+  if (text === "") {
+    return;
+  }
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -16,8 +45,36 @@ const handleSubmit = (event) => {
       text,
     }),
   });
+
+  if (response.status === 201) {
+    const { newCommentId } = await response.json();
+    textarea.value = "";
+    addComment(text, newCommentId);
+  }
 };
 
-if (form) {
-  form.addEventListener("submit", handleSubmit);
+const handleDelete = async (event) => {
+  const commentList = event.target.parentNode;
+  const commentId = commentList.dataset.id;
+  const videoId = videoContainer.dataset.id;
+  const response = await fetch(`/api/comment/${commentId}/delete`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      videoId,
+    }),
+  });
+  if (response.status === 201) {
+    deleteComment(event);
+  }
+  if (response.status === 403) {
+    alert("not your comments");
+  }
+};
+
+form.addEventListener("submit", handleSubmit);
+for (let i = 0; i < deleteBtn.length; i++) {
+  deleteBtn[i].addEventListener("click", handleDelete);
 }
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,5 @@
 import Video from "../models/Video";
+import Comment from "../models/Comment";
 import User from "../models/User";
 
 export const home = async (req, res) => {
@@ -10,7 +11,7 @@ export const home = async (req, res) => {
 
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id).populate("owner");
+  const video = await Video.findById(id).populate("owner").populate("comments");
   console.log("video", video);
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
@@ -131,9 +132,50 @@ export const registerView = async (req, res) => {
   return res.sandStatus(200);
 };
 
-export const createComment = (req, res) => {
-  console.log(req.params);
-  console.log(req.body);
-  console.log(req.body.text, req.body.rating);
-  return res.end();
+export const createComment = async (req, res) => {
+  const {
+    session: { user },
+    body: { text },
+    params: { id },
+  } = req;
+
+  const video = await Video.findById(id);
+
+  if (!video) {
+    return res.sendStatus(404);
+  }
+  const commentUser = await User.findById(user._id);
+  const comment = await Comment.create({
+    text,
+    owner: user._id,
+    video: id,
+  });
+
+  video.comments.push(comment._id);
+  commentUser.comments.push(comment._id);
+  commentUser.save();
+  video.save();
+  req.session.user = commentUser;
+  return res.status(201).json({ newCommentId: comment._id });
+};
+
+export const deleteComment = async (req, res) => {
+  const {
+    params: { id },
+    body: { videoId },
+    session: { user },
+  } = req;
+  const video = await Video.findById(videoId);
+  const commentUser = await User.findById(user._id);
+  if (user.comments.indexOf(id) < 0) {
+    req.flash("info", "Not authorized");
+    return res.sendStatus(403);
+  }
+  commentUser.comments.splice(commentUser.comments.indexOf(id), 1);
+  video.comments.splice(video.comments.indexOf(id), 1);
+  await video.save();
+  await commentUser.save();
+  await Comment.findByIdAndDelete(id);
+
+  return res.sendStatus(201);
 };
